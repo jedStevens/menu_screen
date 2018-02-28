a5634b3c7e17f983adc016aa10b12231f44731a0
Jed Stevens <jed.w.stevens@gmail.com>
2018-02-28 14:37:48 -0500

    Added Water + Play Button

--- main_menu.gd
+++ main_menu.gd
@@ -1,22 +1,8 @@
 extends Control
 
-var percentage = 0
+func go_to_game():
+	get_tree().change_scene("res://game.tscn")
 
-var steps = ["Downloading Data...", "Preparing Game...", "Loading World...", "Loading Server...", "Done!"]
 
-
-func _ready():
-	set_process(true)
-
-func _process(time):
-	percentage += time * 5
-	
-	var info =  min(int(percentage / 25), len(steps))
-	
-	get_node("progress_info").text = steps[info]
-	
-	get_node("progress").value = percentage
-	
-	if get_node("progress").value == get_node("progress").max_value:
-		get_tree().change_scene("res://game.tscn")
-		
+func _on_play_pressed():
+	go_to_game()
